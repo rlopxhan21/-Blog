826264e2d1a792d2075c9f304b019e6672b9745a
R Lopxhan <rlopxhan21@gmail.com>
2022-12-27 13:01:31 +1100

    Remaining navbar responisve

--- frontend/src/components/Blog/BlogItem.js
+++ frontend/src/components/Blog/BlogItem.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import classes from "./BlogItem.module.scss";
 
 const BlogItem = (props) => {
@@ -5,20 +7,28 @@ const BlogItem = (props) => {
     <div className={classes.blogitem}>
       <article>
         <div className={classes.left}>
-          <p>{props.area}</p>
-          <h3>{props.topic.substring(0, 75)}</h3>
+          <Link to="/" className={classes.area}>
+            <p>{props.area}</p>
+          </Link>
+          <Link to="/">
+            <h3>{props.topic.substring(0, 75)}</h3>
+          </Link>
           <div className={classes["author-section"]}>
             <div>
               <img src={require("../../assets/images/profile.jpg")} alt={""} />
             </div>
             <div>
-              <p>{props.author}</p>
+              <Link to="/" className={classes.authorname}>
+                {props.author}
+              </Link>
               <p>7{props.published_date}</p>
             </div>
           </div>
         </div>
         <div className={classes.middle}>
-          <p>{props.content.substring(0, 400)}</p>
+          <Link to="/">
+            <p>{props.content.substring(0, 400)}</p>
+          </Link>
         </div>
         <div className={classes.right}>
           <img src={require("../../assets/images/profile.jpg")} alt={""} />
