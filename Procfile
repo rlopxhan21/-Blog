2b3ef11ab1c263a4b8891c6ad5a5adc77a69504e
R Lopxhan <rlopxhan21@gmail.com>
2022-12-31 18:42:30 +1100

    Project ready for applying auth in react app

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: python manage.py migrate
-web: gunicorn Blog.wsgi --log-file -
--- backend/Blog/settings.py
+++ backend/Blog/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lopxhan-blog.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = []
 
 
 # Application definition
